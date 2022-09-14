da16f0055b4b7feaea7a377145acd3f21de0a159
Juanma Lopez <juanmalm86@gmail.com>
2022-09-14 15:51:02 +0200

    first commit

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiApp1;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+        // Replace previous line with next one to check how NewPage1 should look.
+        // MainPage = new NewPage1();
+    }
+}
